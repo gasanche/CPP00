4d4244b9607304f15fefe89a859b827bdccdc699
gabsanch <130669207+gasanche@users.noreply.github.com>
2024-04-28 22:52:15 +0000

    comienza la aventura

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (EXIT_SUCCESS);
+	}
+	for()
+}
