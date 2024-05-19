c6b28f01cc47cdeda183b20c7241592ea5707ed9
Gabriela Camila Sanchez Espinoza <gabsanch@cbr8s4.42barcelona.com>
2024-05-19 19:17:06 +0200

    [ADD] ex01: PhoneBook.hpp and Contact.hpp

---
+++ ex01/Contact.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabsanch <gabsanch@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 18:46:14 by gabsanch          #+#    #+#             */
+/*   Updated: 2024/05/19 19:15:09 by gabsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+
+# define CONTACT_HPP
+
+class Contact{
+	int		Index;
+	std::string	FirstName;
+	std::string	LastName;
+	std::string	Nickname
+};
+
+#endif
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabsanch <gabsanch@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 18:33:58 by gabsanch          #+#    #+#             */
+/*   Updated: 2024/05/19 18:45:57 by gabsanch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+
+# define PHONEBOOK_HPP
+
+#include <iostream>
+#include  <string>
+
+class PhoneBook{
+	private:
+
+
+	public:
+
+};
+
+#endif
