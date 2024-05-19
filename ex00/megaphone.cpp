6849ace274f043e60cdfcf9ea13971fce4fe3dac
Gabriela Camila Sanchez Espinoza <gabsanch@cbr8s4.42barcelona.com>
2024-05-19 18:09:17 +0200

    [ADD] ex00 and .gitignore

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -8,5 +8,9 @@ int	main(int argc, char **argv)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (EXIT_SUCCESS);
 	}
-	for()
+	for(int	i = 1; i < argc; i++)
+		for(int j = 0; j < strlen(argv[i]); j++)
+			std::cout << (char)toupper(argv[i][j]);
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
 }
--- hola.cpp
+++
@@ -1,3 +0,0 @@
-dsdjbaijkjsavfbvjbkdakjbjshipadv
-asfklkHJDKVH,VJWSDA
-
